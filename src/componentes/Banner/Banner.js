05588214cb31df7469f4c1a22d841b98c69f8f78
Davi Solari <119772568+solari-davi@users.noreply.github.com>
2022-12-21 02:36:02 -0300

    Adicionando os arquivos do projeto

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    //JSX
+    return (
+        <header className='banner'>
+            <img src="/imgs/banner.png" alt="O banner principal da página da Organo"/>
+        </header>
+    )
+}
+
